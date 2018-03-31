47aa076aeac812fa2020a1eb59a7e8a552864560
webwaffle <37251044+webwaffle@users.noreply.github.com>
2018-03-31 15:34:45 -0400

    Lots of stuff
    Added:
    -sidebar
    -styling create post button
    -div for posts
    -show own account posts in sidebar

--- home.php
+++ home.php
@@ -9,6 +9,7 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
     </head>
 <body>
+<div id="main">
 <?php
 $file="[" . substr(file_get_contents("posts.json"), 0, -1) . "]";
 $array_of_posts=json_decode($file);
@@ -20,8 +21,22 @@ foreach(array_reverse($array_of_posts) as $current) {
     echo('<p class="timetext">Posted on ' . $current->time . '<br>By ' . $current->poster . '</p><p class="posttext">' . $current->text . '</p></div>');
 }
 ?>
+</div>
 	<div id="createpostdiv">
     	<a href="create-post.php"><p id="createpostlink">Create Post</p></a>
+        <?php
+    $file="[" . substr(file_get_contents("posts.json"), 0, -1) . "]";
+    $array_of_posts=json_decode($file);
+    foreach(array_reverse($array_of_posts) as $current) {
+        if ($current->poster == $_SESSION["username"]) {
+            echo('<div class="postdiv">');
+            if (isset($current->title)) {
+                echo('<a href="posts.php?post=' . $current->title . '">' . '<h1 class="posttitle">' . $current->title . "</h1></a>");
+            }
+        echo('<p class="timetext">Posted on ' . $current->time . '<br>By ' . $current->poster . '</p><p class="posttext">' . $current->text . '</p></div>');
+    }
+}
+?>
 	</div>
     </body>
 </html>
