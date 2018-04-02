447f67eeffa29b443333cf8a093e45a9338f390c
webwaffle <37251044+webwaffle@users.noreply.github.com>
2018-04-02 18:27:39 -0400

    fixed .gitignore

---
+++ user-create.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Site</title>
+    <link rel="stylesheet" type="text/css" href="css/forms.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+</head>
+<body>
+	<form class="formarea" action="user-create-process.php" method="POST">
+		<p>Username</p>
+		<input type="text" name="username"><br>
+		<p>Password</p>
+		<input type="password" name="password"><br>
+		<input type="submit" value="Create User">
+	</form>
+</body>
+</html>
