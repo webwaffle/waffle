98aefc27bc587faaaf58d607041661aee2544cb5
Unknown <sam525watchman@gmail.com>
2018-04-14 13:50:09 -0400

    moved to include()
    moved topbar to topbar.html and sidebar to sidebar.php

--- user.php
+++ user.php
@@ -7,39 +7,13 @@
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
 </head>
 <body>
-    <nav>
-        <h1 style="text-align: center;">Waffle</h1>
-        <ul>
-            <li><a class="topbarlink" href="home.php">Home</a></li>
-            <li><a class="topbarlink" href="https://github.com/webwaffle/waffle">Code</a></li>
-            <li><a class="topbarlink" href="logout.php">Logout</a></li>
-            <li><a class="topbarlink" href="about.php">About</a></li>
-        </ul>
-    </nav>
-    
+  <?php include("topbar.html"); ?>
+
+
     <div id="main">
-        
+
     </div>
-    
-    <div id="createpostdiv">
-    	<a href="create-post.php"><p id="createpostlink">Create Post</p></a>
-        <?php
-    $file="[" . substr(file_get_contents("posts.json"), 0, -1) . "]";
-    $array_of_posts=json_decode($file);
-    foreach(array_reverse($array_of_posts) as $current) {
-        if ($current->poster == $_SESSION["username"]) {
-            echo('<div class="postdiv">');
-            if (isset($current->title)) {
-                echo('<a href="posts.php?post=' . $current->title . '">' . '<h1 class="posttitle">' . $current->title . "</h1></a>");
-            }
-        echo('<p class="timetext">Posted on ' . $current->time . '<br>By <a style="color:grey; display: inline;" href="user.php?user=' . $current->poster . '">' . $current->poster . '</a></p><p class="posttext">' . $current->text . '</p>');
-        if (isset($current->file_ext)) {
-            echo('<img width="32" height="32" class="imgpage" src="imguploads/' . $current->title . $current->file_ext . '" />');
-        }
-        echo("</div>");
-    }
-}
-?>
-	</div>
+
+    <?php include("sidebar.php"); ?>
     </body>
 </html>
