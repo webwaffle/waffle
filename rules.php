76d0578b935611d773ed4f3b768f8c079d9a90bd
webwaffle <sam525watchman@gmail.com>
2018-05-01 18:54:14 -0400

    Finished rules

--- rules.php
+++ rules.php
@@ -11,9 +11,15 @@
 <h1>Rules for waffle</h1>
 <h4>Stay organic!</h4>
 <p>Here at Waffle we are commited to having a good community. Follow these rules, and you'll have a good time.</p>
-<h2>1. Be appropriate</h2>
+<h2>#1. Be appropriate</h2>
 <p>If you post anything innapropriate the post WILL be taken down and you could get banned.</p>
-<h2>2. Be nice</h2>
-<p>You don't have to be </p>
+<h2>#2. Be nice</h2>
+<p>You don't have to be super nice to everyone, but if you are super <i>mean</i> to everyone, that's a problem.</p>
+<h2>#3. Ignore #4</h2>
+<h2>#4. Don't ignore #3</h2>
+<h2>#5. No reposts</h2>
+<p>If it's already been posted, <i>don't post it.</i></p>
+<h1>Punishments</h1>
+<p>You have 5 chances. Breaking Rule #1 and in extreme cases #2 will use 2. Breaking #5 and sometimes #2 will use 1. <br> Of course, I reserve the right to ban you if your case is too extreme.</p>
 </body>
 </html>
--- user-create-process.php
+++ user-create-process.php
@@ -4,13 +4,20 @@ date_default_timezone_set("America/New_York");
 if ($_POST["username"] == "" || $_POST["password"] == "") {
 	header("Location: index.html");
 }
+if(!(isset($_POST["rules"]))) {
+	echo("You didn't agree to the rules.");
+	$agreed = FALSE;
+} else {
+	$agreed = TRUE;
+}
 $array = [];
 $array["username"] = $_POST["username"];
 $array["password"] = $_POST["password"];
 $friends = [];
 $array["friends"] = $friends;
 $json_string = json_encode($array, JSON_PRETTY_PRINT);
-if(file_put_contents("json/users.json", $json_string . ",", FILE_APPEND)) {
+if($agreed) {
+	file_put_contents("json/users.json", $json_string . ",", FILE_APPEND);
 	$_SESSION["username"] = $_POST["username"];
 	$logfile = fopen("log.txt", "a");
 	$name = $_SESSION["username"];
@@ -18,6 +25,4 @@ if(file_put_contents("json/users.json", $json_string . ",", FILE_APPEND)) {
 	fwrite($logfile, "[LOGIN]: User $name created and logged in at $time.\n");
     header("Location: home.php");
 }
-else
-	echo("error");
 ?>
