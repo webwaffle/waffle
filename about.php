719d43da8e18621175d566994d2dbbc414079db0
webwaffle <sam525watchman@gmail.com>
2018-05-06 18:30:41 -0400

    Worked on about

--- about.php
+++ about.php
@@ -1,7 +1,25 @@
+<?php session_start(); ?>
 <html>
 <head>
+<title>About Waffle</title>
 <link rel="stylesheet" type="text/css" href="css/main.css" />
 <link rel="stylesheet" type="text/css" href="css/home.css" />
-<link rel="stylesheet" type="text/css" href="css/.css" />
+<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet" />
+<script src="js/lib/jquery.js"></script>
+<style>
+    .centered {
+        text-align: center;
+    }
+    .link {
+        text-decoration: underline;
+        display: inline;
+        color: black;
+    }
+</style>
 </head>
+<body>
+    <h1 class="darktext centered" id="slogan">Waffle: 100% Organic Social Media</h1>
+    <p class="darktext centered">Waffle is a free, <a href="https://github.com/webwaffle/waffle" class="link">open source</a> social media site.<br>
+    We do not share your data with any third parties. <br></p>
+</body>
 </html>
